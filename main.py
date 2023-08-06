3bb6f28e48de0691a4607655afa42d2da19b5c62
Dany Valdez <99849047+MagnusTerra@users.noreply.github.com>
2023-08-06 13:20:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
 
-# Replace book.pdf with any pdf of your choice
-loader = UnstructuredPDFLoader("book.pdf")
+loader = UnstructuredPDFLoader("C.pdf")
 pages = loader.load_and_split()
 embeddings = OpenAIEmbeddings()
 docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
 
-# Choose any query of your choice
-query = "Who is Rich Dad?"
-docs = docsearch.get_relevant_documents(query)
-# chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
-chain = load_qa_chain(ChatOpenAI(temperature=0), chain_type="stuff")
-output = chain.run(input_documents=docs, question=query)
-print(output)
+while True:
+    query = str(input("Escribe tu pregunta: "))
+    if query == "salir":
+        break
+    
+    docs = docsearch.get_relevant_documents(query)
+
+    chain = load_qa_chain(ChatOpenAI(temperature=0), chain_type="stuff")
+    output = chain.run(input_documents=docs, question=query)
+    print(output)
