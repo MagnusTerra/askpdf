e8f1702eae20ec597823eed75cbbe6fd72bb45f2
Dany Valdez <99849047+MagnusTerra@users.noreply.github.com>
2023-08-06 13:30:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
 
-loader = UnstructuredPDFLoader("C.pdf")
-pages = loader.load_and_split()
-embeddings = OpenAIEmbeddings()
-docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
-
-while True:
-    query = str(input("Escribe tu pregunta: "))
-    if query == "salir":
-        break
+def ask_pdf(book:str ):
+    loader = UnstructuredPDFLoader(book)
+    pages = loader.load_and_split()
+    embeddings = OpenAIEmbeddings()
+    docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
     
-    docs = docsearch.get_relevant_documents(query)
-
-    chain = load_qa_chain(ChatOpenAI(temperature=0), chain_type="stuff")
-    output = chain.run(input_documents=docs, question=query)
-    print(output)
+    while True:
+        query = str(input("Escribe tu pregunta: "))
+        if query == "salir":
+            break
+        
+        docs = docsearch.get_relevant_documents(query)
+    
+        chain = load_qa_chain(ChatOpenAI(temperature=0), chain_type="stuff")
+        output = chain.run(input_documents=docs, question=query)
+        print(output)
